6fdef278432361e244aaf2b2e2829d9bbc234944
weihan <30489517+weihanAu@users.noreply.github.com>
2025-04-28 22:54:20 +1000

    Update improvement.js

--- improvement.js
+++ improvement.js
@@ -16,13 +16,22 @@ const generateAllMeters = () => {
 const authHeader = req.headers['authorization']; // 或 req.get('Authorization')
 let token = null;
 
-if (authHeader && authHeader.startsWith('Bearer ')) {
-  token = authHeader.split(' ')[1]; // 提取 Bearer 后面的 token
-}
+app.use((req, res, next) => {
+  const authHeader = req.headers['authorization'];
+
+  if (authHeader && authHeader.startsWith('Bearer ')) {
+    const token = authHeader.split(' ')[1];
+    req.token = token;
+    next();
+  } else {
+    res.status(401).json({ message: 'Not authorized' });
+  }
+});
+
 // 1 missing error handler middle ware {Add Error Handling Middleware}
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ error: "Something went wrong!" });
+  res.status(err.statusCode||500).json({ error: err.message });
 });
 // 2 missing status code return
 // 3 no imput validation
